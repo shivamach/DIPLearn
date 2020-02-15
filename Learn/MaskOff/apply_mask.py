cfe8a2a5af91747ee9194357c04348d12c7fb883
shivamach <shivamach13@gmail.com>
2020-02-15 16:50:50 +0530

    mask is going no where, need an academic approach for this

---
+++ Learn/MaskOff/apply_mask.py
@@ -0,0 +1,19 @@
+import cv2
+import imutils
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-m", "--mask", default="./emptyform.jpg", help="path to input mask")
+ap.add_argument("-i", "--image",required=True,help="path to image which will get the mask")
+args = vars(ap.parse_args())
+# imports done and arguments parsed
+
+mask = cv2.imread(args["mask"])
+mask = imutils.resize(mask, width=500)
+image = cv2.imread(args["image"])
+image = imutils.resize(image, width=500)
+
+output = cv2.bitwise_or(image, image , mask=mask)
+
+cv2.imshow("output", mask)
+cv2.waitKey(0)
